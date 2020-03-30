744d957f61a197718317e9e5917c4f8e82e79513
VVugo <adrien.brest01@gmail.com>
2020-03-30 15:32:34 +0200

    Test unitaire

---
+++ UnitTestGSBCR/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestGSBCR")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestGSBCR")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31342ce0-e73e-41f0-a6b4-a66c106837c3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestGSBCR/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GSBCR.modele;
+using GSBCR.BLL;
+using System.Collections.Generic;
+
+namespace UnitTestGSBCR
+{
+    [TestClass]
+    public class UnitVisiteurManager
+    {
+        [TestMethod]
+        public void TestChargerVisiteur()
+        {
+            VISITEUR v = VisiteurManager.ChargerVisiteur("a131", "30BFD069");
+            Assert.IsNotNull(v, "le visiteur a131 mdp 30BFD069 non chargé");
+            VISITEUR v1 = VisiteurManager.ChargerVisiteur("a131", "");
+            Assert.IsNull(v1, "le visiteur a131 est chargé sans mot de passe");
+        }
+
+        [TestMethod]
+        public void TestChargerRapportVisiteurEnCours()
+        {
+            bool ok = false;
+            List<RAPPORT_VISITE> lr = VisiteurManager.ChargerRapportVisiteurEncours("a131");
+            foreach (RAPPORT_VISITE r in lr)
+            {
+                Assert.AreEqual("a131", r.RAP_MATRICULE, "le rapport n''appartient pas au matricule a131");
+                Assert.AreEqual("1", r.RAP_ETAT, "état rapport différent de en cours (1)");
+                ok = (r.RAP_NUM >= 57 && r.RAP_NUM <= 62);
+                Assert.IsTrue(ok, "n° de rapport faux");
+            }
+
+        }
+        [TestMethod]
+        public void TestChargerAffectationVisiteur()
+        {
+
+        }
+    }
+}
