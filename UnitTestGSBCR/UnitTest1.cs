ebc75f58ce113664fe2715406a1c9b1bd69e7771
VVugo <adrien.brest01@gmail.com>
2020-03-30 17:22:56 +0200

    Formulaire Windows

--- UnitTestGSBCR/UnitTest1.cs
+++ UnitTestGSBCR/UnitTest1.cs
@@ -17,6 +17,12 @@ namespace UnitTestGSBCR
             VISITEUR v1 = VisiteurManager.ChargerVisiteur("a131", "");
             Assert.IsNull(v1, "le visiteur a131 est chargé sans mot de passe");
         }
+        [TestMethod]
+        public void TestChargerAffectationVisiteur()
+        {
+            VAFFECTATION vaf = VisiteurManager.ChargerAffectationVisiteur("a131");
+            Assert.AreEqual("27/05/1996 00:00:00", vaf.JJMMAA, "L'affectation de a131 n'est pas la dernière");
+        }
 
         [TestMethod]
         public void TestChargerRapportVisiteurEnCours()
@@ -33,9 +39,34 @@ namespace UnitTestGSBCR
 
         }
         [TestMethod]
-        public void TestChargerAffectationVisiteur()
+        public void TestChargerRapportVisite()
         {
-
+            RAPPORT_VISITE rap = VisiteurManager.ChargerRapportVisite("a131", 3);
+            Assert.AreEqual("Médecin curieux, à recontacer en décembre pour réunion", rap.RAP_BILAN, "Le rapport n'est pas le 3 du visiteur a 131");
         }
+
+        [TestMethod]
+        public void TestChargerRapportVisiteurFinis()
+        {
+            bool ok = false;
+            List<RAPPORT_VISITE> lr = VisiteurManager.ChargerRapportVisiteurFinis("a131");
+            foreach (RAPPORT_VISITE r in lr)
+            {
+                Assert.AreEqual("a131", r.RAP_MATRICULE, "le rapport n''appartient pas au matricule a131");
+                Assert.AreEqual("2", r.RAP_ETAT, "état rapport différent de non lus (2)");
+               //Assert.AreEqual("3", r.RAP_ETAT, "état rapport différent de terminé (3)");
+                ok = (r.RAP_NUM >= 57 && r.RAP_NUM <= 62);
+                Assert.IsTrue(ok, "n° de rapport faux");
+            }
+        }
+
+        [TestMethod]
+        public void TestChargerVisiteurByRegion()
+        {
+            List<VISITEUR> lv = VisiteurManager.ChargerVisiteurByRegion("BN");
+        }
+
+        
+    
     }
 }
